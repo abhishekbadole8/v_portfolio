48e2154b1f3ee1ec0acb7f351471dba1bac1f24a
abhishekbadole8 <abhishekbadole12@gmail.com>
2024-05-19 19:50:58 +0530

    feat: about page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,7 +3,21 @@ import { cardo, inter } from "../fonts";
 import Image from "next/image";
 
 function Home() {
+  const aboutAray = [
+    { title: "YEARS OF EXPERIENCE", value: "19", icon: "/icons/Stack.svg" },
+    { title: "COMPLETED PROJECTS", value: "514", icon: "/icons/Briefcase.svg" },
+    { title: "HAPPY CLIENTS", value: "494", icon: "/icons/SmileyWink.svg" },
+    { title: "AWARDS WINNERS", value: "52", icon: "/icons/Medal.svg" },
+  ];
 
+  const clients = [
+    { name: "Amazon", path: "/amazon.svg" },
+    { name: "LinkedIn", path: "/linkedin.svg" },
+    { name: "Medium", path: "/medium.svg" },
+    { name: "Dribbble", path: "/dribbble.svg" },
+    { name: "Spotify", path: "/spotify.svg" },
+    { name: "Tumblr", path: "/tumblr.svg" },
+  ];
 
   return (
     <div>
@@ -47,6 +61,119 @@ function Home() {
         </div>
       </div>
 
+      {/* About Page */}
+      <div className="py-10 px-40 relative">
+        <div className="flex justify-between ">
+          {/* First column */}
+          <div className="flex-none w-1/4">
+            {/* Biography Section */}
+            <div className="mb-12">
+              <h5
+                className={`${cardo.className} font-bold text-2xl leading-8 text-gray-900 mb-4`}
+              >
+                Biography
+              </h5>
+              <p
+                className={`${inter.className} font-normal text-base leading-6 text-gray-500`}
+              >
+                Vestibulum tempor enim metus, quis tempor urna facilisis sed.
+                Sed nibh ipsum, molestie cursus dolor vitae, molestie vestibulum
+                quam.{" "}
+              </p>
+            </div>
+
+            {/* My Services Section */}
+            <div className="mb-12">
+              <h5
+                className={`${cardo.className} font-bold text-2xl leading-8 text-gray-900 mb-4`}
+              >
+                My Services
+              </h5>
+              <ul
+                className={`${inter.className} font-normal text-base leading-6 text-gray-500 list-disc pl-5`}
+              >
+                <li className="mb-2">User Experience Design</li>
+                <li className="mb-2">User Interface Design</li>
+                <li>Usability Testing</li>
+              </ul>
+            </div>
+
+            {/* My Locations Section */}
+            <div className="mb-12">
+              <h5
+                className={`${cardo.className} font-bold text-2xl leading-8 text-gray-900 mb-4`}
+              >
+                My Locations
+              </h5>
+              <p
+                className={`${inter.className} font-normal text-base leading-6 text-gray-500`}
+              >
+                4517 San Francisco Ave. Manchester, Kentucky 39495, Los Angeles,
+                California, United States.
+              </p>
+            </div>
+          </div>
+
+          {/* Middle column */}
+          <div className="grow absolute -top-52">
+            {/* <Image
+              src={"/avatar/Photo.svg"}
+              alt="user"
+              width={550}
+              height={550}
+            /> */}
+            <div className="bg-gray-400" />
+          </div>
+
+          {/* Third column */}
+          <div className="flex-none w-1/4 ">
+            {aboutAray.map((item, index) => (
+              <div
+                key={index}
+                className={`flex items-start justify-end gap-6 ${
+                  index !== aboutAray.length - 1 ? "mb-12" : ""
+                }`}
+              >
+                <div className="flex flex-col gap-3">
+                  <h2
+                    className={`${cardo.className} text-gray-900 font-normal text-5xl leading-10 text-right `}
+                  >
+                    {item.value}+
+                  </h2>
+                  <p
+                    className={`${inter.className} text-gray-400 font-normal text-sm leading-5 text-right`}
+                  >
+                    {item.title}
+                  </p>
+                </div>
+                <Image src={item.icon} alt="icon" width={45} height={45} />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <p
+          className={`${cardo.className} text-center font-normal text-2xl leading-6 text-gray-900 my-12`}
+        >
+          My clients all over the world
+        </p>
+
+        <div className="flex justify-center items-center gap-10">
+          {clients.map((client, index) => (
+            <div
+              key={index}
+              className="w-48 h-14  flex justify-center items-center"
+            >
+              <Image
+                src={`/logo${client.path}`}
+                alt="client-logo"
+                width={113}
+                height={24}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
